fc5bdc871659898c9ac0e1a2a0b5b06b846ad271
jojoscode <forjodene-code@yahoo.com>
2017-08-20 19:00:47 -0500

    Commit html, css, reset css, js, images etc.

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+S(document).ready(function(){
+  $('.carousel').slick({
+    SlidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
