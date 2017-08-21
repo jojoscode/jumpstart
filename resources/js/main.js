13ff0b7f8be7bd04dd49f0b2e6ae75c2cc7e9ab8
jojoscode <forjodene-code@yahoo.com>
2017-08-21 16:57:59 -0500

    added final js to project

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,4 +8,17 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false
   });
+
+  var farUserScrolled = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - farUserScrolled > 50){
+      var navbarHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navbarHeight}, 150);
+      farUserScrolled = scrollTop;
+    } else if (farUserScrolled - scrollTop > 50){
+      $('.navbar').animate({top: '0px'}, 150);
+      farUserScrolled = scrollTop;
+    }
+  });
 });
